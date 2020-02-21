b8e83b2a984b94b9eb14cd6ef3742e39a74db736
Nikita Sirons <kit.shiron@gmail.com>
2020-02-21 13:45:04 +0100

    Init Dataset and DataLoader

--- data.py
+++ data.py
@@ -1,6 +1,15 @@
 import os
 import wget
 import zipfile
+import cv2
+
+
+import numpy as np
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
 
 
 def download_data_pkg(cur_dir, dataset_name,  dataset_type):
@@ -28,4 +37,71 @@ def download_all_data():
     download_data_pkg(cur_dir, dataset_name='PhC-C2DH-U373', dataset_type='challenge')
 
 
-download_all_data()
+def elastic_transform(image, alpha, sigma, random_state=None):
+    """Elastic deformation of images as described in [Simard2003]_.
+    .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
+       Convolutional Neural Networks applied to Visual Document Analysis", in
+       Proc. of the International Conference on Document Analysis and
+       Recognition, 2003.
+       source: https://gist.github.com/erniejunior/601cdf56d2b424757de5
+    """
+    if random_state is None:
+        random_state = np.random.RandomState(None)
+
+    shape = image.shape
+    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+    dz = np.zeros_like(dx)
+
+    x, y, z = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), np.arange(shape[2]))
+
+    indices = np.reshape(y+dy, (-1, 1)), np.reshape(x+dx, (-1, 1)), np.reshape(z, (-1, 1))
+
+    distored_image = map_coordinates(image, indices, order=1, mode='reflect')
+    return distored_image.reshape(image.shape)
+
+
+class ImageDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        self.image = []
+        self.target = []
+
+        n = len(os.listdir(root_dir)) // 3
+        for i in range(1, n+1):
+            image_folder = os.path.join(root_dir, f"0{i}")
+            target_folder = os.path.join(os.path.join(root_dir, f"0{i}_GT", "SEG"))
+            image_names = [filename.replace('man_seg', 't') for filename in os.listdir(target_folder)]
+            self.image.extend(cv2.imread(os.path.join(image_folder, image_name), -1) for image_name in image_names)
+            self.target.extend(cv2.imread(os.path.join(target_folder, filename), -1) for filename in os.listdir(target_folder))
+
+    def __len__(self):
+        return len(self.image)
+
+    def __getitem__(self, idx):
+        return transforms.ToTensor()(self.image[idx]), transforms.ToTensor()(self.target[idx] / 65535)
+
+
+cur_dir = os.path.abspath('')
+root_dir = os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training")
+transformed_dataset = ImageDataset(root_dir)
+dataloader = DataLoader(transformed_dataset, batch_size=4,
+                        shuffle=True, num_workers=4)
+
+
+# import cv2
+# import numpy as np
+# cur_dir = os.path.abspath('')
+# print(os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training", "01_GT", "SEG", "man_seg067.tif"))
+# img = cv2.imread(os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training", "01_GT", "SEG", "man_seg005.tif"), -1)
+# img_scaled = cv2.normalize(img, dst=None, alpha=0, beta=65535, norm_type=cv2.NORM_MINMAX)
+# print(np.max(img_scaled))
+# cv2.imshow("hello", img_scaled)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    download_all_data()  # run this to get data
