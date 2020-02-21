b40dbd98062d89c8fa5fe8d31f42b20f312d5892
Nikita Sirons <kit.shiron@gmail.com>
2020-02-21 17:52:12 +0100

    data augment update

--- data.py
+++ data.py
@@ -43,22 +43,22 @@ def elastic_transform(image, alpha, sigma, random_state=None):
        Convolutional Neural Networks applied to Visual Document Analysis", in
        Proc. of the International Conference on Document Analysis and
        Recognition, 2003.
-       source: https://gist.github.com/erniejunior/601cdf56d2b424757de5
+       source: https://gist.github.com/chsasank/4d8f68caf01f041a6453e67fb30f8f5a
     """
+    assert len(image.shape) == 2
+
     if random_state is None:
         random_state = np.random.RandomState(None)
 
     shape = image.shape
+
     dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
     dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
-    dz = np.zeros_like(dx)
 
-    x, y, z = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), np.arange(shape[2]))
+    x, y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), indexing='ij')
+    indices = np.reshape(x + dx, (-1, 1)), np.reshape(y + dy, (-1, 1))
 
-    indices = np.reshape(y+dy, (-1, 1)), np.reshape(x+dx, (-1, 1)), np.reshape(z, (-1, 1))
-
-    distored_image = map_coordinates(image, indices, order=1, mode='reflect')
-    return distored_image.reshape(image.shape)
+    return map_coordinates(image, indices, order=1).reshape(shape)
 
 
 class ImageDataset(Dataset):
@@ -75,20 +75,14 @@ class ImageDataset(Dataset):
             target_folder = os.path.join(os.path.join(root_dir, f"0{i}_GT", "SEG"))
             image_names = [filename.replace('man_seg', 't') for filename in os.listdir(target_folder)]
             self.image.extend(cv2.imread(os.path.join(image_folder, image_name), -1) for image_name in image_names)
-            self.target.extend(cv2.imread(os.path.join(target_folder, filename), -1) for filename in os.listdir(target_folder))
+            self.target.extend(cv2.normalize(cv2.imread(os.path.join(target_folder, filename), -1), dst=None, alpha=0, beta=65535, norm_type=cv2.NORM_MINMAX) for filename in os.listdir(target_folder))
 
     def __len__(self):
         return len(self.image)
 
     def __getitem__(self, idx):
-        return transforms.ToTensor()(self.image[idx]), transforms.ToTensor()(self.target[idx] / 65535)
-
-
-cur_dir = os.path.abspath('')
-root_dir = os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training")
-transformed_dataset = ImageDataset(root_dir)
-dataloader = DataLoader(transformed_dataset, batch_size=4,
-                        shuffle=True, num_workers=4)
+        return transforms.ToTensor()(elastic_transform(self.image[idx], alpha=3, sigma=10)), \
+               transforms.ToTensor()(elastic_transform(self.target[idx], alpha=3, sigma=10))
 
 
 # import cv2
@@ -105,3 +99,7 @@ dataloader = DataLoader(transformed_dataset, batch_size=4,
 
 if __name__ == '__main__':
     download_all_data()  # run this to get data
+    cur_dir = os.path.abspath('')
+    root_dir = os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training")
+    transformed_dataset = ImageDataset(root_dir)
+    dataloader = DataLoader(transformed_dataset, batch_size=4, shuffle=True, num_workers=4)
