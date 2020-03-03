62eacecefeaddbd472e764606ec7745bfa6a61f4
perecanals <56351022+perecanals@users.noreply.github.com>
2020-03-03 22:04:21 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,65 @@
+import cv2 as cv
+import numpy as np
+import torch
+
+def weighted_map(gt):
+
+    """ This method is needed to compute the weight map for each ground truth (gt) segmentation, to compensate the different frequency of pixels for each class. 
+        With this method, we highlight the borders between different objects (cells in the case of the HeLa dataset used in Ronneberger et al. 2015) 
+
+        Input:
+            - gt: ground truth, generated segmentation mask (0,1). Torch tensor
+
+        Output: 
+            - w: weight map. Torch tensor (same shape as gt)
+
+    """
+
+    # Hyperparameters given by Ronneberger et al. 2015
+    w0 = 10
+    sig2 = 25 # 5^2
+
+    # The unique command returns the unique values inside the input tensor, in these case 0 and 1, and these are stored in "uval"
+    # "counts" includes the number of times we find each value in "uval" inside of the input tensor
+    [uval, counts] = torch.unique(gt, return_counts=True)
+
+    # For the coputation of the w_c tensor:
+    w_c = torch.empty(gt.shape)
+    for pos in range(len(uval)):
+        # We normalize all pixel values according to the class frequencies. We also constrain that the cell class is set to 1
+        w_c[gt == uval[pos]] = counts[1].float() / counts[pos].float() 
+
+    # Now we have to highlight borders with the second element from formula (2) from Ronneberger et al. (2015)
+    gt_img = gt.view(gt.shape[1], -1)
+    gt_img = gt_img.numpy()
+
+    # Identifies separated objects present in gt and gives each one a different value
+    n_obj, objects = cv.connectedComponents(gt_img.astype(np.uint8), connectivity=4)
+
+    # In order to compute the distance map for each object, we separate these on different channels
+    objects_sep = np.zeros([n_obj-1, gt_img.shape[0], gt_img.shape[1]])
+    for ii in range(n_obj-1):
+        objects_sep[ii, :, :] = objects==ii+1
+
+    # Computes the distance transform, i.e. distance to the closest zero-value pixel for each identified object
+    # Notice that we are subtracting 1 from 
+    maps = np.zeros([n_obj-1, gt_img.shape[0], gt_img.shape[1]])
+    for ii in range(n_obj-1):
+        maps[ii,: , :] = cv.distanceTransform(objects_sep[ii, :, :].astype(np.uint8)-1, cv.DIST_L2, maskSize=0)
+
+    # We sort the distance maps along the channels (objects) and we then keep d1 and d2, defined as the distance the 
+    # closest two objects for each background pixel
+    maps = np.sort(maps, 0)
+    d1 = maps[0, :, :]
+    d2 = maps[1, :, :]
+
+    # We now compute the w_d matrix following formula (2) in Ronneberger et al. 2015
+    w_d = np.zeros(gt_img.shape)
+    w_d = np.multiply(w0 * np.exp(-np.square(d1+d2) / (2 * sig2)), -1 * (gt - 1)) # We only compute background pixels
+
+    # Finally we add w_c and w_d to generate the final weights
+    w = w_c + w_d
+
+    w = w.clone().detach()
+
+    return w
--- network.py
+++ network.py
@@ -6,6 +6,16 @@ import torchvision
 import torchvision.transforms as transforms
 
 class Unet(nn.Module):
+
+""" Unet 2D implementation (Ronneberger et al. 2015)
+
+    This network reproduces the Unet architecture described in Ronneberger et al. 2015.
+    The structure of this network is made up of 23 convolutional layers including pooling 
+    operation and ReLU activation functions, as well as transpose convolutions. We can 
+    identify a contractive path and an expansive path with skip connections between both.
+
+    Possible improvements: BN, padding to conv layers
+"""
     def __init__(self):
         super(Unet, self).__init__() # (what does this do?)
         self.conv11c = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3)
@@ -23,32 +33,20 @@ class Unet(nn.Module):
         self.conv51c = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3)
         self.conv52c = nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3)
 
-        # self.upsamp4 = nn.UpsamplingBilinear2d(size=(28,28), scale_factor=2) # Do we have to input each channel separately? 2d or 3d?
-        # self.upconv4 = nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=2)
-
         self.upconv4 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
 
         self.conv41e = nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=3)
         self.conv42e = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3)
-
-        # self.upsamp3 = nn.UpsamplingBilinear2d(size=(52,52), scale_factor=2) 
-        # self.upconv3 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=2)
 
         self.upconv3 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
 
         self.conv31e = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3)
         self.conv32e = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3)
 
-        # self.upsamp2 = nn.UpsamplingBilinear2d(size=(100,100), scale_factor=2) 
-        # self.upconv2 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=2)
-
         self.upconv2 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
 
         self.conv21e = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3)
         self.conv22e = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3)
-
-        # self.upsamp1 = nn.UpsamplingBilinear2d(size=(196,196), scale_factor=2) 
-        # self.upconv1 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=2)
 
         self.upconv1 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
 
@@ -58,13 +56,25 @@ class Unet(nn.Module):
         self.finalconv = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1)
 
     def crop_and_concat(self, A, B):
+        """ Crop and concatenation (for skip connections)
+            
+            We use this simple function to crop the tensor (H and W) from the contractive 
+            path to the size of that from the expansive path and concatenate both. We need 
+            this method for the skip connections found in the Unet architecture.
+
+            Inputs:
+                - A: tensor from the contractive path.
+                - B: tensor from the expansive path.
+
+            Output:
+                - Concatenated (axis=1) tensor 
+        """
         crop_factor=(A.size()[2] - B.size()[2]) * 0.5
         c=int(crop_factor)
         A = F.pad(A, (-c, -c, -c, -c))
         return torch.cat((A,B), 1)
 
     def forward(self,t):
-
         # First level contractive convolutions
         t = F.relu(self.conv11c(t))
         t = F.relu(self.conv12c(t))
@@ -94,42 +104,34 @@ class Unet(nn.Module):
         t = F.relu(self.conv52c(t))
 
         # Upconvolution 4
-        # t = self.upsamp4(t) # Dealing with dimensions
         t = self.upconv4(t)
         t = self.crop_and_concat(t4, t)
 
         # Fourth level expansive convolutions
-
         t = F.relu(self.conv41e(t))
         t = F.relu(self.conv42e(t))
 
         # Upconvolution 3
-        # t = self.upsamp3(t) # Dealing with dimensions
         t = self.upconv3(t)
         t = self.crop_and_concat(t3, t)
 
         # Third level expansive convolutions
-
         t = F.relu(self.conv31e(t))
         t = F.relu(self.conv32e(t))
 
         # Upconvolution 2
-        # t = self.upsamp2(t) # Dealing with dimensions
         t = self.upconv2(t)
         t = self.crop_and_concat(t2, t)
 
         # Second level expansive convolutions
-
         t = F.relu(self.conv21e(t))
         t = F.relu(self.conv22e(t))
 
         # Upconvolution 1
-        # t = self.upsamp1(t) # Dealing with dimensions
         t = self.upconv1(t)
         t = self.crop_and_concat(t1, t)
 
         # First level expansive convolutions
-
         t = F.relu(self.conv11e(t))
         t = F.relu(self.conv12e(t))
 
