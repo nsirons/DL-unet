373b424de120d8cb762bd2f9873cd69f521dedaa
Nikita Sirons <kit.shiron@gmail.com>
2020-03-05 00:25:39 +0100

    isbi2012 segmented, mirror, input 572x572, output 388x388

--- data.py
+++ data.py
@@ -1,15 +1,10 @@
 import os
 import wget
 import zipfile
-import cv2
+import cv2 as cv
 
 from PIL import Image
 import numpy as np
-from scipy.ndimage.interpolation import map_coordinates
-from scipy.ndimage.filters import gaussian_filter
-
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
 
 
 def download_data_pkg(cur_dir, dataset_name,  dataset_type):
@@ -55,7 +50,6 @@ def download_isbi(cur_dir, dataset_type):
                     os.mkdir(os.path.join(cur_dir, "data", folder_name, name, "SEG"))
 
                 img = Image.open(os.path.join(cur_dir, "data", folder_name, f"{t}-{folder}.tif"))
-
                 i = 0
                 while True:
                     try:
@@ -63,7 +57,17 @@ def download_isbi(cur_dir, dataset_type):
                         if folder == 'volume':
                             img.save(os.path.join(cur_dir, "data", folder_name, name, f't{i:03d}.tif'))
                         else:
-                            img.save(os.path.join(cur_dir, "data", folder_name, name, 'SEG', f'man_seg{i:03d}.tif'))
+                            #  https://stackoverflow.com/questions/29130255/load-tiff-image-as-numpy-array
+                            dtype = {'F': np.float32, 'L': np.uint8}[img.mode]
+
+                            # Load the data into a flat numpy array and reshape
+                            np_img = np.array(img.getdata(), dtype=dtype)
+                            w, h = img.size
+                            np_img.shape = (h, w)
+
+                            _, np_img = cv.connectedComponents(np_img)
+                            # img.save(os.path.join(cur_dir, "data", folder_name, name, 'SEG', f'man_seg{i:03d}.tif'))
+                            cv.imwrite(os.path.join(cur_dir, "data", folder_name, name, 'SEG', f'man_seg{i:03d}.tif'), np_img)
                         i += 1
                     except EOFError:
                         # Not enough frames in img
@@ -84,100 +88,5 @@ def download_all_data():
     download_isbi(cur_dir, "challenge")
 
 
-def elastic_transform(image, alpha, sigma, random_state=None):
-    """Elastic deformation of images as described in [Simard2003]_.
-    .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
-       Convolutional Neural Networks applied to Visual Document Analysis", in
-       Proc. of the International Conference on Document Analysis and
-       Recognition, 2003.
-       source: https://gist.github.com/chsasank/4d8f68caf01f041a6453e67fb30f8f5a
-    """
-    assert len(image.shape) == 2
-
-    if random_state is None:
-        random_state = np.random.RandomState(None)
-
-    shape = image.shape
-
-    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
-    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
-
-    # TODO: change to bicubic interpolation
-    x, y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), indexing='ij')
-    indices = np.reshape(x + dx, (-1, 1)), np.reshape(y + dy, (-1, 1))
-
-    return map_coordinates(image, indices, order=1).reshape(shape)
-
-
-class ImageDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
-        self.transform = transform
-
-        self.image = []
-        self.target = []
-
-        n = len(os.listdir(root_dir)) // 3
-        for i in range(1, n+1):
-            image_folder = os.path.join(root_dir, f"0{i}")
-            target_folder = os.path.join(os.path.join(root_dir, f"0{i}_GT", "SEG"))
-            image_names = [filename.replace('man_seg', 't') for filename in os.listdir(target_folder)]
-            self.image.extend(cv2.imread(os.path.join(image_folder, image_name), -1) for image_name in image_names)
-            for filename in os.listdir(target_folder):
-                img = cv2.imread(os.path.join(target_folder, filename), -1)
-                img[img > 0] = 1.
-                self.target.append(img)
-
-    def __len__(self):
-        return len(self.image)
-
-    def __getitem__(self, idx):
-        return transforms.ToTensor()(elastic_transform(self.image[idx], alpha=3, sigma=10)), \
-               transforms.ToTensor()(elastic_transform(self.target[idx], alpha=3, sigma=10))
-
 if __name__ == "__main__":
     download_all_data()
-    print('hello')
-# import cv2
-# import numpy as np
-# cur_dir = os.path.abspath('')
-# print(os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training", "01_GT", "SEG", "man_seg067.tif"))
-# img = cv2.imread(os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training", "01_GT", "SEG", "man_seg005.tif"), -1)
-# img_scaled = cv2.normalize(img, dst=None, alpha=0, beta=65535, norm_type=cv2.NORM_MINMAX)
-# print(np.max(img_scaled))
-# cv2.imshow("hello", img_scaled)
-#
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# if __name__ == '__main__':
-#     import cv2
-
-#     import numpy as np
-#     cur_dir = os.path.abspath('')
-#     print(os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training", "01_GT", "SEG", "man_seg067.tif"))
-#     img = cv2.imread(os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training", "01_GT", "SEG", "man_seg005.tif"), -1)
-#     # img_scaled = cv2.normalize(img, dst=None, alpha=0, beta=65535, norm_type=cv2.NORM_MINMAX)
-#     img[img > 0] = 65535
-#     img_scaled = img
-#     print(np.max(img_scaled), np.min(img_scaled))
-#     cv2.imshow("hello", img_scaled)
-
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#     exit()
-#     # run this to download data
-#     download_all_data()
-
-#     cur_dir = os.path.abspath('')
-#     root_dir = os.path.join(cur_dir, "data", "DIC-C2DH-HeLa-training")
-#     transformed_dataset = ImageDataset(root_dir)
-
-#     # Testing
-#     img, mask = transformed_dataset[0]
-#     total = np.hstack((img.numpy().reshape((512, 512)), mask.numpy().reshape(512,512)))
-#     cv2.imshow("hello", total)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-
-#     dataloader = DataLoader(transformed_dataset, batch_size=4, shuffle=True, num_workers=4)
